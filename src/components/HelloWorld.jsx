5c6cdace85ccac5af1d5ad90259088afc1ff6dbd
mallorysmith64 <mallorysmith64@gmail.com>
2020-09-26 01:24:08 -0400

    added routing home to page1, started list on home

--- src/components/HelloWorld.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function HelloWorld() {
-  return <>Hello, World!</>
-}
-
-export default HelloWorld
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-import HelloWorld from '../components/HelloWorld'
+// import React, { useState } from 'react'
+import Page from './Page'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
-function HomePage() {
-  return <HelloWorld />
+const HomePage = () => {
+  // const [a,b] = useState([])
+
+  return (
+    <>
+      <Router>
+        <Link to="/">Home</Link>
+        <section>
+          <ul>
+            <li>
+              <Link to="/Page">Stack</Link>
+            </li>
+            <li>djdj</li>
+            <li>lorem ipsum</li>
+          </ul>
+        </section>
+        <Route path="/Page" component={Page} />
+      </Router>
+    </>
+  )
 }
-
 export default HomePage
