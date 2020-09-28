d978350636e857737a7a9595c13e167b69cc339b
mallorysmith64 <mallorysmith64@gmail.com>
2020-09-28 02:15:21 -0400

    make my own paper component via css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const HomePage = () => {
+function HomePage() {
   return (
     <>
-      <section>
-        <header>Datastructures</header>
-        <ul>
-          <li>
-            <Link to="/2">Stack</Link>
-          </li>
-          <li>
-            <Link to="/3">Queues</Link>
-          </li>
-          <li>
-            <Link to="/4">Linked Lists</Link>
-          </li>
-        </ul>
-      </section>
+      <div className="paper-container">
+        <section className="paper-content">
+          <h4>Data Structures</h4>
+          <ul>
+            <li>
+              <Link to="/2">Stack</Link>
+            </li>
+            <li>
+              <Link to="/3">Queues</Link>
+            </li>
+            <li>
+              <Link to="/4">Linked Lists</Link>
+            </li>
+          </ul>
+        </section>
+      </div>
     </>
   )
 }
-
 export default HomePage
