2b1d804070103c203cfcb8f4ce513b33278cb13b
mallorysmith64 <mallorysmith64@gmail.com>
2020-09-28 22:59:40 -0400

    added code editor via npm react-ace

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import AceEditor from 'react-ace'
+import 'ace-builds/src-noconflict/mode-java'
+import 'ace-builds/src-noconflict/theme-github'
+import 'ace-builds/src-noconflict/ext-language_tools'
 
 function Page2() {
+  function onChange(newValue) {
+    console.log('change', newValue)
+  }
+
   return (
-    <section className="header">
-      <header>Stack (Array Implementation)</header>
-    </section>
+    <>
+      <section className="header">
+        <header>Stack (Array Implementation)</header>
+      </section>
+
+      <AceEditor
+        mode="java"
+        theme="github"
+        onChange={onChange}
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{ $blockScrolling: true }}
+        setOptions={{
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: true,
+          enableSnippets: true,
+        }}
+      />
+      );
+    </>
   )
 }
 
