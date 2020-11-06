f84eaa1ff2ea9bac0b272f9a5dc4502910f4e2c7
mallorysmith64 <mallorysmith64@gmail.com>
2020-11-06 06:44:47 -0500

    added tracking for user input, push button addes values to squares on first click

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AceEditor from 'react-ace'
 import 'ace-builds/src-noconflict/ext-language_tools'
 import 'ace-builds/webpack-resolver'
@@ -7,11 +7,20 @@ import PlayArrowIcon from '@material-ui/icons/PlayArrow'
 import ArrowLeftIcon from '@material-ui/icons/ArrowLeft'
 import ArrowRightIcon from '@material-ui/icons/ArrowRight'
 
-import Squares from '../components/Squares'
-
 function Page2() {
-  function onChange(newValue) {
-    console.log('changed array', newValue)
+  let [push, setPush] = useState('')
+  let [submitted, setSubmitted] = useState(false)
+
+  const handleChange = event => {
+    console.log(event.target.value)
+    setPush(event.target.value)
+  }
+
+  const submitChange = event => {
+    event.preventDefault()
+    console.log('changed')
+    setSubmitted(true)
+    setPush(push)
   }
 
   return (
@@ -44,13 +53,19 @@ function Page2() {
         </div>
 
         <section className="operation-container">
-          <div className="input">
-            <input type="text" placeholder="enter num or char"></input>
-          </div>
-
-          <div className="buttons">
-            <Button variant="contained">Push</Button>
-          </div>
+          <form onSubmit={submitChange}>
+            <div className="input">
+              <input
+                type="text"
+                onChange={handleChange}
+                value={push}
+                placeholder="enter num or char"
+              ></input>
+            </div>
+          </form>
+          <Button onClick={submitChange} value="Submit" variant="contained">
+            Push
+          </Button>
 
           <div className="buttons">
             <Button variant="contained">Pop</Button>
@@ -72,7 +87,6 @@ function Page2() {
         <AceEditor
           mode="javascript"
           theme="twilight"
-          onChange={onChange}
           name="editor"
           placeholder="Happy Coding!"
           fontSize={18}
@@ -85,10 +99,21 @@ function Page2() {
         />
 
         <section className="squares-container">
-          <Squares />
-          <Squares />
-          <Squares />
-          <Squares />
+          <button className="square">
+            {submitted === true ? push[0] : ''}
+          </button>
+
+          <button className="square">
+            {submitted === true ? push[1] : ''}
+          </button>
+
+          <button className="square">
+            {submitted === true ? push[2] : ''}
+          </button>
+
+          <button className="square">
+            {submitted === true ? push[3] : ''}
+          </button>
         </section>
       </section>
     </>
